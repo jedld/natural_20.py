062533569c61a1862552ef834537c4e665d4fc9a
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2025-07-28 11:25:47 +0800

    More bug fixes

--- webapp/start_web.sh
+++ webapp/start_web.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-TEMPLATE_DIR=../templates python -m flask run --debug --host=0.0.0.0 --port=5001
+# Use command-line argument as TEMPLATE_DIR if provided, otherwise use default
+if [ -n "$1" ]; then
+    TEMPLATE_DIR="$1"
+else
+    TEMPLATE_DIR="../templates"
+fi
+
+TEMPLATE_DIR="$TEMPLATE_DIR" python -m flask run --debug --host=0.0.0.0 --port=5001
